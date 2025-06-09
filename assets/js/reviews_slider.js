7d03071d34e73d5a2dfb729fce564f195af05c8a
MrsIgnis <igneis@yandex.ru>
2025-06-09 20:45:42 +1000

    Сделан footer блок, скрыты prev слайды в слайдерах

--- assets/js/reviews_slider.js
+++ assets/js/reviews_slider.js
@@ -7,4 +7,28 @@ const rev_swiper = new Swiper('.reviews_swiper', {
         nextEl: '.reviews_button_next',
         prevEl: '.reviews_button_prev',
     },
-})
+
+    on: {
+        init: function () {
+            updateSlidesVisibility(this);
+        },
+        slideChange: function () {
+            updateSlidesVisibility(this);
+        },
+    },
+});
+
+function updateSlidesVisibility(swiperInstance) {
+    const slides = swiperInstance.slides;
+    const activeIndex = swiperInstance.activeIndex;
+
+    slides.forEach((slide, index) => {
+        if (index < activeIndex) {
+            slide.style.opacity = '0';
+            slide.style.visibility = 'hidden';
+        } else {
+            slide.style.opacity = '1';
+            slide.style.visibility = 'visible';
+        }
+    });
+}
